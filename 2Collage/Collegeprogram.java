c92cb2a229dd17c6097111689b9223fc5445340d
Jelaica <44886225+Jelaica@users.noreply.github.com>
2018-11-23 17:55:05 +0800

    Update and rename Collageprogram.java to Collegeprogram.java

--- 2Collage/Collegeprogram.java
+++ 2Collage/Collegeprogram.java
@@ -1,7 +1,7 @@
 
-package collageprogram;
+package collegeprogram;
 import java.util.Scanner;
-public class Collageprogram {
+public class Collegeprogram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello Welocome to the University of Cordilleras:");
